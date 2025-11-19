471aa75d4ff17f866f544374c4970779a68ba71b
Cha Seojun <sjcha36@gmail.com>
2025-11-19 23:42:26 +0900

    change the directory name and fix indentation

--- 05_branching/branch.s
+++ 05_branching/branch.s
@@ -1,11 +1,9 @@
-	.text
-	.global _start
-
+  .text
+  .global _start
 _start:
-	mov r0, #2     @ r0 = 2
-	b foo          @ Jump to foo (pc = foo)
-	mov r1, r0     @ never executed
-
+  mov r0, #2       @ r0 = 2
+  b foo            @ Jump to foo (pc = foo)
+  mov r1, r0       @ never executed
 foo:               @ Label foo
-	add r0, r0, #3   @ r0 = 5
-	b _start         @ infinite loop (pc = _start)
+  add r0, r0, #3   @ r0 = 5
+  b _start         @ infinite loop (pc = _start)
